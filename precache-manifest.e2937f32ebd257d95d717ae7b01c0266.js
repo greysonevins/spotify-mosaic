6f60f4f3947bdafd353de3fbd462df7053bac6e0
greysonevins <greyman1995@gmail.com>
2020-03-11 17:56:02 -0400

    Updates

---
+++ precache-manifest.e2937f32ebd257d95d717ae7b01c0266.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3b7daf9e6575889d31c94c7c3fa22d3",
+    "url": "/spotify-mosaic/index.html"
+  },
+  {
+    "revision": "20b657063064fe9ee647",
+    "url": "/spotify-mosaic/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "cea92f5992761c74321f",
+    "url": "/spotify-mosaic/static/js/2.4592b28e.chunk.js"
+  },
+  {
+    "revision": "84c7b19269f6e118b33bddb7c7a89789",
+    "url": "/spotify-mosaic/static/js/2.4592b28e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a849eea87ff49cfa9364",
+    "url": "/spotify-mosaic/static/js/3.fa584bc4.chunk.js"
+  },
+  {
+    "revision": "20b657063064fe9ee647",
+    "url": "/spotify-mosaic/static/js/main.5dea736f.chunk.js"
+  },
+  {
+    "revision": "7cc9538dee5cc1098f2b",
+    "url": "/spotify-mosaic/static/js/runtime-main.1101e849.js"
+  }
+]);
